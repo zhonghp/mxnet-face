eb2fa4cf2fa7dea3282f84bff3dc87a0ab37df56
Huaping Zhong <zhonghp@foxmail.com>
2017-01-11 18:13:59 +0800

    add db module

--- identification/face_api.py
+++ identification/face_api.py
@@ -3,137 +3,203 @@
 # @Author: vasezhong
 # @Date:   2017-01-10 15:24:34
 # @Last Modified by:   vasezhong
-# @Last Modified time: 2017-01-10 19:46:03
+# @Last Modified time: 2017-01-11 17:38:09
 
-import cv2
+import os
 import numpy as np
-import mxnet as mx
 
+import util
 import config
 from align_face import AlignDlib
-from lightened_cnn import lightened_cnn_b_feature
+from vgg_feature_extractor import VggFeatureExtractor
+from lightened_cnn_feature_extractor import LightenedCnnFeatureExtractor
+
 
 class FaceAPI:
-  def __load_exector(self):
-    _, model_args, model_auxs = mx.model.load_checkpoint(config.model_prefix, config.epoch)
-    symbol = lightened_cnn_b_feature()
-    return symbol, model_args, model_auxs
 
-  def __bgr2gray(self, img_bgr):
-    if img_bgr is not None:
-      img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-    else:
-      img_gray = None
-    return img_gray
+    def __init__(self):
+        if config.extractor == 'vgg_face':
+            self.__extractor = VggFeatureExtractor()
+        else:
+            self.__extractor = LightenedCnnFeatureExtractor()
+        self.__db = []
+        self.__align = AlignDlib(config.dlib_model_file)
 
-  def __rgb2gray(self, img_rgb):
-    if img_rgb is not None:
-      img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-    else:
-      img_gray = None
-    return img_gray
+    def __del__(self):
+        self.__db = []
 
-  def __bgr2rgb(self, img_bgr):
-    if img_bgr is not None:
-      img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-    else:
-      img_rgb = None
-    return img_rgb
+    def align_face(self, img_bgr, bbox):
+        img_rgb = util.bgr2rgb(img_bgr)
+        return self.__align.align(config.face_size, img_rgb, bbox, ts=config.ts)
 
-  def __rgb2bgr(self, img_rgb):
-    if img_rgb is not None:
-      img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
-    else:
-      img_bgr = None
-    return img_bgr
+    def detect_all_faces(self, img_bgr):
+        img_rgb = util.bgr2rgb(img_bgr)
+        return self.__align.getAllFaceBoundingBoxes(img_rgb)
 
-  def __init__(self):
-    self.__align = AlignDlib(config.dlib_model_file)
-    self.__symbol, self.__model_args, self.__model_auxs = self.__load_exector()
+    def detect_largest_face(self, img_bgr):
+        img_rgb = util.bgr2rgb(img_bgr)
+        return self.__align.getLargestFaceBoundingBox(img_rgb)
 
-  def align_face(self, img_bgr, bbox):
-    img_rgb = self.__bgr2rgb(img_bgr)
-    return self.__align.align(config.face_size, img_rgb, bbox, ts=config.ts)
+    def __get_face_feature(self, img_bgr, bbox):
+        face_rgb = self.align_face(img_bgr, bbox)
+        face_bgr = util.rgb2bgr(face_rgb)
+        return self.__extractor.extract_feature(face_bgr)
 
-  def detect_all_faces(self, img_bgr):
-    img_rgb = self.__bgr2rgb(img_bgr)
-    return self.__align.getAllFaceBoundingBoxes(img_rgb)
+    def clear_db(self):
+        self.__db = []
 
-  def detect_largest_face(self, img_bgr):
-    img_rgb = self.__bgr2rgb(img_bgr)
-    return self.__align.getLargestFaceBoundingBox(img_rgb)
+    def __feature_str_to_feature(self, feature_str):
+        segs = feature_str.decode('utf-8').split('\t')
+        assert(len(segs) == 2)
+        label = segs[0].strip()
+        feature_str = segs[1].strip()
+        feature_vec = [float(feature_val)
+                       for feature_val in feature_str.split(',')]
+        feature = np.ndarray(shape=(config.feature_size, ),
+                             dtype=float, buffer=np.array(feature_vec))
+        return feature, label
 
-  def verify(self, img1_bgr, img2_bgr):
-    result = []
-    bboxes1 = self.detect_all_faces(img1_bgr)
-    bboxes2 = self.detect_all_faces(img2_bgr)
+    def __add_a_feature_str_to_db(self, feature_str):
+        feature_with_label = self.__feature_str_to_feature(feature_str)
+        self.__db.append(feature_with_label)
 
-    pair_arr = np.zeros((2, 1, config.face_size, config.face_size), dtype=float)
-    # img1 = np.expand_dims(self.__rgb2gray(img1_bgr), axis=0)
-    # img2 = np.expand_dims(self.__rgb2gray(img2_bgr), axis=0)
-    # pair_arr[0][:] = img1/255.0
-    # pair_arr[1][:] = img2/255.0
-    # self.__model_args['data'] = mx.nd.array(pair_arr, config.ctx)
-    # exector = self.__symbol.bind(config.ctx, self.__model_args ,args_grad=None, grad_req="null", aux_states=self.__model_auxs)
-    # exector.forward(is_train=False)
-    # exector.outputs[0].wait_to_read()
-    # output = exector.outputs[0].asnumpy()
-    # return np.dot(output[0], output[1])/np.linalg.norm(output[0])/np.linalg.norm(output[1])
+    def load_db(self, filename="train.db"):
+        if filename and not os.path.exists(filename):
+            return
+        self.clear_db()
+        if not filename:
+            return
+        with open(filename, 'r') as reader:
+            for line in reader.readlines():
+                self.__add_a_feature_str_to_db(line.strip())
 
-    for bbox1 in bboxes1:
-      dist = []
-      face1_rgb = self.align_face(img1_bgr, bbox1)
-      face1_gray = self.__rgb2gray(face1_rgb)
-      face1 = np.expand_dims(face1_gray, axis=0)
-      assert(face1.shape == (1, config.face_size, config.face_size))
-      pair_arr[0][:] = face1/255.0
-      for bbox2 in bboxes2:
-        face2_rgb = self.align_face(img2_bgr, bbox2)
-        face2_gray = self.__rgb2gray(face2_rgb)
-        face2 = np.expand_dims(face2_gray, axis=0)
-        assert(face2.shape == (1, config.face_size, config.face_size))
-        pair_arr[1][:] = face2/255.0
+    def __search_db(self, feature, max_num, threshold):
+        score_list = []
+        for index, (db_feature, db_label) in enumerate(self.__db):
+            similarity = util.cosine_similarity(feature, db_feature)
+            if similarity > threshold:
+                score_list.append((index, db_label, similarity))
+        if len(score_list) == 0:
+            return 0, []
+        value_to_cmp = lambda pair: pair[2]
+        sorted_score_list = sorted(
+            score_list, key=value_to_cmp, reverse=True)
+        if max_num > len(sorted_score_list):
+            max_num = len(sorted_score_list)
+        return max_num, sorted_score_list[:max_num]
 
-        self.__model_args['data'] = mx.nd.array(pair_arr, config.ctx)
-        exector = self.__symbol.bind(config.ctx, self.__model_args ,args_grad=None, grad_req="null", aux_states=self.__model_auxs)
-        exector.forward(is_train=False)
-        exector.outputs[0].wait_to_read()
-        output = exector.outputs[0].asnumpy()
-        dis = np.dot(output[0], output[1])/np.linalg.norm(output[0])/np.linalg.norm(output[1])
-        dist.append(dis)
-      result.append(dist)
-    return result
+    def __search_db_by_face(self, img_bgr, bbox, max_num, threshold):
+        feature = self.__get_face_feature(img_bgr, bbox)
+        return self.__search_db(feature, max_num, threshold)
+
+    def search_db(self, img_bgr, max_num=3, threshold=1e-9):
+        all_score_list = []
+        bboxes = self.detect_all_faces(img_bgr)
+        for bbox in bboxes:
+            count, score_list = self.__search_db_by_face(
+                img_bgr, bbox, max_num, threshold)
+            all_score_list.extend(score_list)
+        if len(all_score_list) == 0:
+            return 0, []
+        value_to_cmp = lambda pair: pair[2]
+        sorted_score_list = sorted(
+            all_score_list, key=value_to_cmp, reverse=True)
+        if max_num > len(sorted_score_list):
+            max_num = len(sorted_score_list)
+        return max_num, sorted_score_list[:max_num]
+
+    def __feature_to_str(self, feature, label):
+        feature_str = [unicode(feature_val)
+                       for feature_val in feature.flatten()]
+        return u"{}\t{}".format(label, ','.join(feature_str)).encode('utf-8')
+
+    def gen_feature_str_from_db(self):
+        for labeled_feature in self.__db:
+            yield self.__feature_to_str(*labeled_feature)
+
+    def save_db(self, filename="train.db"):
+        with open(filename, 'w') as writer:
+            for feature_str in self.gen_feature_str_from_db():
+                writer.write(feature_str)
+                writer.write('\n')
+
+    def add_face_to_db(self, img_bgr, bbox, label):
+        if isinstance(label, str):
+            label = label.decode('utf-8')
+        feature = self.__get_face_feature(img_bgr, bbox)
+        self.__db.append((feature, label))
+
+    def build_db(self, root):
+        self.clear_db()
+        for subdir, dirs, files in os.walk(root):
+            for path in files:
+                (label, fname) = (os.path.basename(subdir), path)
+                (name, ext) = os.path.splitext(fname)
+                if ext in config.exts:
+                    print label, subdir, fname
+                    img_bgr = cv2.imread(os.path.join(subdir, fname))
+                    bboxes = self.detect_all_faces(img_bgr)
+                    for bbox in bboxes:
+                        self.add_face_to_db(img_bgr, bbox, label)
+
+    def verify(self, img1_bgr, img2_bgr):
+        result = []
+        bboxes1 = self.detect_all_faces(img1_bgr)
+        bboxes2 = self.detect_all_faces(img2_bgr)
+
+        for bbox1 in bboxes1:
+            dist = []
+            feat1 = self.__get_face_feature(img1_bgr, bbox1)
+            for bbox2 in bboxes2:
+                feat2 = self.__get_face_feature(img2_bgr, bbox2)
+                dist.append(util.cosine_similarity(feat1, feat2))
+            result.append(dist)
+        return result
 
 
 if __name__ == '__main__':
-  import sys
-  if len(sys.argv) != 3:
-    print 'Usage: python face_api.py [image_1] [image_2]'
-    sys.exit(-1)
+    import sys
+    if len(sys.argv) != 3:
+        print 'Usage: python face_api.py [image_1] [image_2]'
+        sys.exit(-1)
 
-  file1 = sys.argv[1].strip()
-  file2 = sys.argv[2].strip()
-  img1 = cv2.imread(file1)
-  img2 = cv2.imread(file2)
-  faceAPI = FaceAPI()
+    import cv2
+    file1 = sys.argv[1].strip()
+    file2 = sys.argv[2].strip()
+    img1 = cv2.imread(file1)
+    img2 = cv2.imread(file2)
 
-  # test verification
-  print faceAPI.verify(img1, img2)
+    faceAPI = FaceAPI()
+    # test verification
+    print faceAPI.verify(img1, img2)
 
-  # test detection
-  img1_faces = faceAPI.detect_all_faces(img1)
-  img2_faces = faceAPI.detect_all_faces(img2)
-  for face in img1_faces:
-    cv2.rectangle(img1, (face.left(), face.top()), (face.right(), face.bottom()), (55,255,155), 1)
-  for face in img2_faces:
-    cv2.rectangle(img2, (face.left(), face.top()), (face.right(), face.bottom()), (55,255,155), 1)
-  cv2.imwrite('test1_detected.jpg', img1)
-  cv2.imwrite('test2_detected.jpg', img2)
+    # # test detection
+    # img1_faces = faceAPI.detect_all_faces(img1)
+    # img2_faces = faceAPI.detect_all_faces(img2)
+    # for face in img1_faces:
+    #     cv2.rectangle(img1, (face.left(), face.top()),
+    #                   (face.right(), face.bottom()), (55, 255, 155), 1)
+    # for face in img2_faces:
+    #     cv2.rectangle(img2, (face.left(), face.top()),
+    #                   (face.right(), face.bottom()), (55, 255, 155), 1)
+    # cv2.imwrite('test1_detected.jpg', img1)
+    # cv2.imwrite('test2_detected.jpg', img2)
 
-  # test alignment
-  img1_face = faceAPI.detect_largest_face(img1)
-  img2_face = faceAPI.detect_largest_face(img2)
-  face1_rgb = faceAPI.align_face(img1, img1_face)
-  face2_rgb = faceAPI.align_face(img2, img2_face)
-  cv2.imwrite('test1_aligned.jpg', cv2.cvtColor(face1_rgb, cv2.COLOR_RGB2BGR))
-  cv2.imwrite('test2_aligned.jpg', cv2.cvtColor(face2_rgb, cv2.COLOR_RGB2BGR))
+    # # test alignment
+    # img1_face = faceAPI.detect_largest_face(img1)
+    # img2_face = faceAPI.detect_largest_face(img2)
+    # face1_rgb = faceAPI.align_face(img1, img1_face)
+    # face2_rgb = faceAPI.align_face(img2, img2_face)
+    # cv2.imwrite('test1_aligned.jpg', cv2.cvtColor(
+    #     face1_rgb, cv2.COLOR_RGB2BGR))
+    # cv2.imwrite('test2_aligned.jpg', cv2.cvtColor(
+    #     face2_rgb, cv2.COLOR_RGB2BGR))
+
+    faceAPI = FaceAPI()
+    faceAPI.build_db('test_db')
+    faceAPI.save_db('test.db')
+
+    faceAPI = FaceAPI()
+    faceAPI.load_db('test.db')
+    print faceAPI.search_db(img1)
+    print faceAPI.search_db(img2)
--- identification/vgg_feature_extractor.py
+++ identification/vgg_feature_extractor.py
@@ -3,40 +3,68 @@
 # @Author: vasezhong
 # @Date:   2017-01-10 23:30:26
 # @Last Modified by:   vasezhong
-# @Last Modified time: 2017-01-11 00:30:02
+# @Last Modified time: 2017-01-11 17:43:19
 
+import copy
 import caffe
 import numpy as np
 
 import config
 from feature_extractor import FeatureExtractor
 
+
 class VggFeatureExtractor(FeatureExtractor):
-	def __init__(self):
-		if config.gpu_id < 0:
-			caffe.set_mode_cpu()
-		else:
-			caffe.set_device(config.gpu_id)
-			caffe.set_mode_gpu()
 
-		self.__net = caffe.Net(config.vgg_model_def, config.vgg_model_weights, caffe.TEST)
-		self.__transformer = caffe.io.Transformer({'data': self.__net.blobs['data'].data.shape})
-		self.__transformer.set_transpose('data', (2, 0, 1))
-		self.__transformer.set_mean('data', np.array(config.mean_val))
-		# self.__transformer.set_raw_scale('data', 255)
-		# self.__transformer.set_channel_swap('data', (2, 1, 0)) # swap channel from RGB to BGR
-		self.__net.blobs['data'].reshape(config.batch_size, config.channel_num, config.face_size, config.face_size)
+    def __init__(self):
+        if config.gpu_id < 0:
+            caffe.set_mode_cpu()
+        else:
+            caffe.set_device(config.gpu_id)
+            caffe.set_mode_gpu()
+        self.__net = caffe.Net(config.vgg_model_def,
+                               config.vgg_model_weights, caffe.TEST)
+        self.__transformer = caffe.io.Transformer(
+            {'data': self.__net.blobs['data'].data.shape})
+        self.__transformer.set_transpose('data', (2, 0, 1))
+        self.__transformer.set_mean('data', np.array(config.mean_val))
+        # self.__transformer.set_raw_scale('data', 255)
+        # self.__transformer.set_channel_swap('data', (2, 1, 0)) # swap channel
+        # from RGB to BGR
+        self.__net.blobs['data'].reshape(
+            config.batch_size, config.channel_num, config.face_size, config.face_size)
 
-	def extract_feature(self, img_bgr):
-		transformed_img = self.__transformer.preprocess('data', img_bgr)
-		self.__net.blobs['data'].data[...] = transformed_img
-		output = self.__net.forward()
-		feature = self.__net.blobs['fc8'].data[0]
-		return feature
+    def extract_feature(self, img_bgr):
+        transformed_img = self.__transformer.preprocess('data', img_bgr)
+        self.__net.blobs['data'].data[...] = transformed_img
+        output = self.__net.forward()
+        feature = self.__net.blobs['fc7'].data[0]
+        return feature.copy()
+
 
 if __name__ == '__main__':
-	import cv2
-	extractor = VggFeatureExtractor()
-	img_bgr = cv2.imread('../model/vgg_face_caffe/ak.png')
-	feature = extractor.extract_feature(img_bgr)
-        print np.argmax(feature)
+    import sys
+    if len(sys.argv) != 3:
+        img1 = 'test1.jpg'
+        img2 = 'test2.jpg'
+    else:
+        img1 = sys.argv[1].strip()
+        img2 = sys.argv[2].strip()
+
+    import cv2
+    import util
+    config.channel_num = 3
+    config.face_size = 224
+    config.feature_size = 4096
+    config.extractor = 'vgg_face'
+
+    extractor = VggFeatureExtractor()
+    # img_bgr = cv2.imread('../model/vgg_face_caffe/ak.png')
+    # feature = extractor.extract_feature(img_bgr)
+
+    img1_bgr = cv2.imread(img1)
+    img2_bgr = cv2.imread(img2)
+    # img1_bgr = cv2.imread('../../../data/lfw-align/Shane_Loux/Shane_Loux_0001.png')
+    # img2_bgr = cv2.imread('../../../data/lfw-align/Val_Ackerman/Val_Ackerman_0001.png')
+    feat1 = extractor.extract_feature(img1_bgr)
+    feat2 = extractor.extract_feature(img2_bgr)
+    print util.cosine_similarity(feat1, feat2)
